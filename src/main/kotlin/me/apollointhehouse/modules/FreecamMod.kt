316a637e0815b0b33488866692e66a5e1be67689
Apollointhehouse <apollocameronboot@gmail.com>
2025-06-21 16:23:23 +1200

    Seperated UI stuff from FreecamMod

--- src/main/kotlin/me/apollointhehouse/modules/FreecamMod.kt
+++ src/main/kotlin/me/apollointhehouse/modules/FreecamMod.kt
@@ -3,41 +3,15 @@ package me.apollointhehouse.modules
 
 import me.apollointhehouse.events.Render3DEvent
 import me.apollointhehouse.mixin.GamemodeAccessor
-import me.apollointhehouse.options.FreecamOptions
 import me.apollointhehouse.raywire.api.EventHandler
 import me.apollointhehouse.raywire.api.event.core.network.PacketEvent
-import me.apollointhehouse.utils.addCallback
-import net.minecraft.client.gui.options.components.BooleanOptionComponent
-import net.minecraft.client.gui.options.components.KeyBindingComponent
-import net.minecraft.client.gui.options.data.OptionsPages
 import net.minecraft.core.net.packet.PacketMovePlayer
 import net.minecraft.core.util.helper.Color
 import net.minecraft.core.util.phys.AABB
 import net.minecraft.core.util.phys.Vec3
-import org.slf4j.LoggerFactory
 
 object FreecamMod : Module() {
 	private var pos = Vec3.getPermanentVec3(0.0,0.0,0.0)
-
-	init {
-		val options = mc.gameSettings as FreecamOptions
-
-		options.freecam.addCallback { option ->
-			if (!option.value) {
-				disable()
-				return@addCallback
-			}
-			enable()
-		}
-
-		options.freecamBind.addCallback {
-			options.freecam.toggle()
-			options.freecam.onUpdate()
-		}
-
-		OptionsPages.GENERAL.withComponent(BooleanOptionComponent(options.freecam))
-		OptionsPages.CONTROLS.withComponent(KeyBindingComponent(options.freecamBind))
-	}
 
 	override fun onEnable() {
 		pos = Vec3.getPermanentVec3(player.x, player.bb.minY, player.z)
@@ -70,6 +44,4 @@ object FreecamMod : Module() {
 
 		event.renderUtils.boxOutline(bb, 2, Color().setRGB(0,0,255))
 	}
-
-	private val logger = LoggerFactory.getLogger(FreecamMod::class.java)
 }
--- src/main/kotlin/me/apollointhehouse/net/client/ClientLogic.kt
+++ src/main/kotlin/me/apollointhehouse/net/client/ClientLogic.kt
@@ -1,17 +1,48 @@
 package me.apollointhehouse.net.client
 
 import me.apollointhehouse.modules.FreecamMod
+import me.apollointhehouse.options.FreecamOptions
 import me.apollointhehouse.raywire.api.EventHandler
 import me.apollointhehouse.raywire.api.event.core.network.PacketEvent
+import me.apollointhehouse.utils.addCallback
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.options.components.BooleanOptionComponent
+import net.minecraft.client.gui.options.components.KeyBindingComponent
+import net.minecraft.client.gui.options.data.OptionsPages
 import net.minecraft.core.net.packet.PacketAESSendKey
 import net.minecraft.core.net.packet.PacketCustomPayload
 import org.slf4j.LoggerFactory
 
 class ClientLogic {
+	val mc = Minecraft.getMinecraft()
+	val options = mc.gameSettings as FreecamOptions
+	var supported = false
+
+	init {
+		options.freecam.addCallback { option ->
+			if (!supported) return@addCallback
+			if (!option.value) {
+				FreecamMod.disable()
+				return@addCallback
+			}
+			FreecamMod.enable()
+		}
+
+		options.freecamBind.addCallback {
+			if (!supported) return@addCallback
+
+			options.freecam.toggle()
+			options.freecam.onUpdate()
+		}
+
+		OptionsPages.GENERAL.withComponent(BooleanOptionComponent(options.freecam))
+		OptionsPages.CONTROLS.withComponent(KeyBindingComponent(options.freecamBind))
+	}
+
 	@EventHandler
 	fun onLogin(event: PacketEvent.Receive) {
 		if (event.packet !is PacketAESSendKey) return
+		supported = false
 		FreecamMod.disable()
 
 		val mc = Minecraft.getMinecraft()
@@ -31,10 +62,11 @@ class ClientLogic {
 		when (val data = packet.data.first()) {
 			0x01.toByte() -> {
 				logger.info("Freecam Supported!")
-				FreecamMod
+				supported = true
 			}
 			else -> {
 				logger.info("Unknown Freecam data received: ${data.toHexString()}")
+				supported = false
 			}
 		}
 	}
