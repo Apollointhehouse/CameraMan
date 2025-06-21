b40749e8b63ff5ff595ca6c2d30ab202f8f9270c
Apollointhehouse <apollocameronboot@gmail.com>
2025-06-21 16:33:38 +1200

    Added a README

--- src/main/kotlin/me/apollointhehouse/utils/KeyBindingExtensions.kt
+++ src/main/kotlin/me/apollointhehouse/utils/KeyBindingExtensions.kt
@@ -3,6 +3,5 @@ package me.apollointhehouse.utils
 import me.apollointhehouse.raywire.Raywire.registry
 import net.minecraft.client.option.KeyBinding
 
-
 fun KeyBinding.addCallback(callback: (KeyBinding) -> Unit) =
 	registry.subscribe(KeybindHandler(this, callback))
